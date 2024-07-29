17c18efff8effd3d700fecb4264963da618c57a4
yashty <yashtyagi2612@gmail.com>
2024-07-29 14:34:09 +0530

    learned about tailwind and props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <Card username = "Vandana"/>
+      <Card username = "Radhika"/>
+      <Card username = "Manju"/>
+      <Card username = "Renu"/>
+    </>
+  )
+}
+
+export default App
+
+
+
